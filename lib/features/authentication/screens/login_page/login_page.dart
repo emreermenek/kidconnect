55ecca97750b85c2a35612f8b4a9bc80621e0f4d
Emre Ermenek <46263471+emreermenek@users.noreply.github.com>
2023-06-11 20:44:12 +0300

    login form field added

--- lib/features/authentication/screens/login_page/login_page.dart
+++ lib/features/authentication/screens/login_page/login_page.dart
@@ -49,7 +49,7 @@ class _LoginPageState extends ConsumerState<LoginPage> {
       resizeToAvoidBottomInset: false,
       body: SizedBox(
         child: Stack(
-          key: _formKey,
+
           children: [
             // arka plandaki şekiller
             const Positioned(
@@ -81,37 +81,38 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                     image:
                         AssetImage('assets/images/login_image/Ellipse 2.png'))),
 
-            Center(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    // kitab animasyon
-                    Lottie.asset(
-                      'assets/animations/lottie_animations/book_animation.json',
-                      width: 250,
-                    ),
+            Form(
+              key: _formKey,
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      // kitab animasyon
+                      Lottie.asset(
+                        'assets/animations/lottie_animations/book_animation.json',
+                        width: 250,
+                      ),
 
-                    // hoşgeldin texti
-                    Text(
-                      'Giriş Yapın',
-                      style: GoogleFonts.gluten(
-                        textStyle: const TextStyle(
-                          color: tBlackColor,
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
+                      // hoşgeldin texti
+                      Text(
+                        'Giriş Yapın',
+                        style: GoogleFonts.gluten(
+                          textStyle: const TextStyle(
+                            color: tBlackColor,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
 
-                    const SizedBox(height: 10),
+                      const SizedBox(height: 10),
 
-                    //TextFeildler
-                    SizedBox(
-                      width: 320,
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            child: TextFormField(
+                      //TextFeildler
+                      SizedBox(
+                        width: 320,
+                        child: Column(
+                          children: [
+                            TextFormField(
                               //email
                               controller: _emailController,
                               textInputAction: TextInputAction.next,
@@ -120,6 +121,7 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                                   vertical: 10,
                                   horizontal: 20,
                                 ),
+
                                 prefixIcon: const Icon(
                                   Icons.email_outlined,
                                   color: tBlackColor,
@@ -141,10 +143,8 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                                 }
                               },
                             ),
-                          ),
-                          const SizedBox(height: 16),
-                          SizedBox(
-                            child: TextFormField(
+                            const SizedBox(height: 16),
+                            TextFormField(
                               controller: _passwordController,
                               textInputAction: TextInputAction.done,
                               obscureText: obscureText,
@@ -183,118 +183,119 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                                 ),
                               ),
                             ),
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          SizedBox(
-                            width: double
-                                .infinity, // Make the button take full width
-                            child: ElevatedButton(
-                              onPressed: () {
-                                if (_formKey.currentState!.validate()) {
-                                  _controller.signInWithEmailAndPassword(
-                                      _emailController, _passwordController);
-                                }
-                              },
-                              style: ElevatedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(25),
+                            const SizedBox(
+                              height: 16,
+                            ),
+                            Center(
+                              child: SizedBox(
+                                width: 100,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    if (_formKey.currentState!.validate()) {
+                                      _controller.signInWithEmailAndPassword(
+                                          _emailController, _passwordController);
+                                    }
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(25),
+                                    ),
+                                    foregroundColor: tWhiteColor,
+                                    backgroundColor: tSecondaryColor,
+                                    padding:
+                                        const EdgeInsets.symmetric(vertical: 12),
+                                  ),
+                                  child: const Text(
+                                    'Giriş',
+                                    style: TextStyle(fontSize: 15),
+                                  ),
                                 ),
-                                foregroundColor: tWhiteColor,
-                                backgroundColor: tSecondaryColor,
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 12),
-                              ),
-                              child: const Text(
-                                'Giriş',
-                                style: TextStyle(fontSize: 15),
                               ),
                             ),
+                          ],
+                        ),
+                      ),
+
+                      const SizedBox(height: 16),
+
+                      Text(
+                        'veya bunlarla giriş yap:',
+                        style: GoogleFonts.comfortaa(
+                            textStyle: const TextStyle(color: tBlackColor)),
+                      ),
+
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              _controller.signInWithFacebook();
+                            },
+                            child: const SizedBox(
+                                width: 80,
+                                child: Image(
+                                  image:
+                                      AssetImage('assets/logo/facebook_logo.png'),
+                                )),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              _controller.signInWithGoogle();
+                            },
+                            child: const SizedBox(
+                                width: 80,
+                                child: Image(
+                                  image:
+                                      AssetImage('assets/logo/google_logo.png'),
+                                )),
                           ),
                         ],
                       ),
-                    ),
 
-                    const SizedBox(height: 16),
+                      const SizedBox(height: 16),
 
-                    Text(
-                      'veya bunlarla giriş yap:',
-                      style: GoogleFonts.comfortaa(
-                          textStyle: const TextStyle(color: tBlackColor)),
-                    ),
-
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        InkWell(
+                      SizedBox(
+                        width: 320,
+                        child: InkWell(
                           onTap: () {
-                            _controller.signInWithFacebook();
+                            Navigator.of(context)
+                                .pushReplacement(MaterialPageRoute(
+                              builder: (context) => const SignupPage(),
+                            ));
                           },
-                          child: const SizedBox(
-                              width: 80,
-                              child: Image(
-                                image:
-                                    AssetImage('assets/logo/facebook_logo.png'),
-                              )),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            _controller.signInWithGoogle();
-                          },
-                          child: const SizedBox(
-                              width: 80,
-                              child: Image(
-                                image:
-                                    AssetImage('assets/logo/google_logo.png'),
-                              )),
-                        ),
-                      ],
-                    ),
-
-                    const SizedBox(height: 16),
-
-                    SizedBox(
-                      width: 320,
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.of(context)
-                              .pushReplacement(MaterialPageRoute(
-                            builder: (context) => const SignupPage(),
-                          ));
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: Row(
-                            children: [
-                              const Expanded(
-                                child: Divider(
-                                  color: Colors.grey,
-                                  thickness: 1,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Row(
+                              children: [
+                                const Expanded(
+                                  child: Divider(
+                                    color: Colors.grey,
+                                    thickness: 1,
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(width: 10),
-                              Text(
-                                'Buradan kayıt ol',
-                                style: GoogleFonts.comfortaa(
-                                    textStyle:
-                                        const TextStyle(color: tBlackColor)),
-                              ),
-                              const SizedBox(width: 10),
-                              const Expanded(
-                                child: Divider(
-                                  color: Colors.grey,
-                                  thickness: 1,
+                                const SizedBox(width: 10),
+                                Text(
+                                  'Buradan kayıt ol',
+                                  style: GoogleFonts.comfortaa(
+                                      textStyle:
+                                          const TextStyle(color: tBlackColor)),
                                 ),
-                              ),
-                            ],
+                                const SizedBox(width: 10),
+                                const Expanded(
+                                  child: Divider(
+                                    color: Colors.grey,
+                                    thickness: 1,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 40),
-                  ],
+                      const SizedBox(height: 40),
+                    ],
+                  ),
                 ),
               ),
             )
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:bootcamp_f32/features/authentication/screens/auth_gate/auth_gate
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 
 
 import 'firebase_options.dart';
