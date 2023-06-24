369cd281a7d682559d3d51d2b393ced7b2ff170b
Emre Ermenek <46263471+emreermenek@users.noreply.github.com>
2023-06-24 08:01:06 +0300

    letter game added
    You can learn alphabet with this game

---
+++ lib/features/app/letter_game/data/backgroundColor.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+List backgroundColors =
+[
+  Colors.blue,Colors.red.shade800,Colors.deepOrangeAccent,Colors.green,Colors.deepPurple,Colors.pinkAccent.shade700,
+  Colors.blue.withBlue(150),Colors.amber, Colors.blue.shade800
+];
+
+List<int> backgroundColorIndex = [
+  /*A*/    /*B*/   /*C*/
+    1,      7,      0,
+  /*Ç*/    /*D*/   /*E*/
+    0,      2,      3,
+  /*F*/    /*G*/   /*Ğ*/
+    4,      5,      5,
+  /*H*/    /*I*/   /*İ*/
+    6,      5,      5,
+  /*J*/    /*K*/   /*L*/
+    3,      1,      8,
+  /*M*/    /*N*/   /*O*/
+    0,      7,      4,
+  /*Ö*/    /*P*/   /*R*/
+    4,      2,      7,
+  /*S*/    /*Ş*/   /*T*/
+    1,      1,      6,
+  /*U*/    /*Ü*/   /*V*/
+    8,      8,      3,
+  /*Y*/    /*Z*/
+    2,      4
+];
