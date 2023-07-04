6ef3f80d55456b90c18372f4fa378a4a0cebbd05
Emre Ermenek <46263471+emreermenek@users.noreply.github.com>
2023-07-04 09:07:15 +0300

    letter choose correct game added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bootcamp_f32/features/app/choose_correct_games/body_choose_correct_game/difficulty.dart';
 import 'package:bootcamp_f32/features/app/choose_correct_games/emotions_choose_correct_game/difficulty.dart';
+import 'package:bootcamp_f32/features/app/choose_correct_games/letter_choose_correct_game/difficulty.dart';
 import 'package:bootcamp_f32/features/app/choose_correct_games/shapes_choose_correct_game/difficulty.dart';
 import 'package:bootcamp_f32/features/app/choose_correct_games/things_choose_correct_game/difficulty.dart';
 import 'package:bootcamp_f32/features/app/choose_correct_games/things_choose_correct_game/easy_things_choose_correct_game.dart';
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
-      home: const EmotionsChooseCorrectGameDifficulty()
+      home: const LetterChooseCorrectGameDifficulty()
     );
   }
 }
